83ddad3264cdffa3d3f7ca54ddc9d5b7b9d59382
dineshsuthar123 <dinesh.suthar18sld@gmail.com>
2025-03-19 23:20:03 +0530

    leetcode daily 3/19/2025

---
+++ src/Questions_march_2025/_3191.java
@@ -0,0 +1,27 @@
+package Questions_march_2025;
+
+public class _3191 {
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 1, 0, 1, 0};
+        MinOperationsSolution solution = new MinOperationsSolution();
+        System.out.println(solution.minOperations(nums));
+    }
+}
+
+class MinOperationsSolution {
+    public int minOperations(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (nums[i] == 0) {
+                nums[i] ^= 1;
+                nums[i + 1] ^= 1;
+                nums[i + 2] ^= 1;
+                count++;
+            }
+        }
+
+        return (nums[n - 2] == 1 && nums[n - 1] == 1) ? count : -1;
+    }
+}
