a67f40eb4bbbe3197b45bb8a43773a1b22529266
dineshsuthar123 <dinesh.suthar18sld@gmail.com>
2025-04-11 20:25:34 +0530

    LeetCode daily 4/11/2025

---
+++ src/Questions_april_2025/_2843.java
@@ -0,0 +1,26 @@
+package Questions_april_2025;
+
+public class _2843 {
+    public static void main(String[] args) {
+        int low = 1000;
+        int high = 9999;
+        SymmetricIntegersSolution solution = new SymmetricIntegersSolution();
+        System.out.println(solution.countSymmetricIntegers(low, high));
+    }
+}
+
+class SymmetricIntegersSolution {
+    public int countSymmetricIntegers(int low, int high) {
+        int count = 0;
+        for (int x = low; x <= high; x++) {
+            String s = String.valueOf(x);
+            int len = s.length();
+            if (len % 2 != 0) continue;
+            int half = len / 2, sum1 = 0, sum2 = 0;
+            for (int i = 0; i < half; i++) sum1 += s.charAt(i) - '0';
+            for (int i = half; i < len; i++) sum2 += s.charAt(i) - '0';
+            if (sum1 == sum2) count++;
+        }
+        return count;
+    }
+}
