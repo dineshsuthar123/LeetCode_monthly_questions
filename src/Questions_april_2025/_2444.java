cf944469afd981e399c4e1d41ff2c33e82824ad4
dineshsuthar123 <dinesh.suthar18sld@gmail.com>
2025-04-26 23:27:35 +0530

    LeetCode daily 4/26/2025

---
+++ src/Questions_april_2025/_2444.java
@@ -0,0 +1,25 @@
+package Questions_april_2025;
+
+public class _2444 {
+    public static void main(String[] args) {
+        int[] nums = {1, 3, 5, 2, 7, 5};
+        int minK = 1;
+        int maxK = 5;
+        CountSubarraysSolution solution = new CountSubarraysSolution();
+        System.out.println(solution.countSubarrays(nums, minK, maxK));
+    }
+}
+
+class CountSubarraysSolution {
+    public long countSubarrays(int[] nums, int minK, int maxK) {
+        long count = 0;
+        int start = -1, mini = -1, maxi = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < minK || nums[i] > maxK) start = i;
+            if (nums[i] == maxK) maxi = i;
+            if (nums[i] == minK) mini = i;
+            count += Math.max(0, Math.min(mini, maxi) - start);
+        }
+        return count;
+    }
+}
