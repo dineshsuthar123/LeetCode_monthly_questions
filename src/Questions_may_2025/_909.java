ce57c8b91548b6b45b4043ba9684f01e43dee924
dineshsuthar123 <dinesh.suthar18sld@gmail.com>
2025-05-31 23:49:26 +0530

    Leetcode 3/15/2025

---
+++ src/Questions_may_2025/_909.java
@@ -0,0 +1,46 @@
+package Questions_may_2025;
+
+import java.util.*;
+
+public class _909 {
+    public static void main(String[] args) {
+        int[][] board = {
+                {-1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1},
+                {-1, -1, -1, -1, -1, -1},
+                {-1, 35, -1, -1, 13, -1},
+                {-1, -1, -1, -1, -1, -1},
+                {-1, 15, -1, -1, -1, -1}
+        };
+        SnakesAndLaddersSolution solution = new SnakesAndLaddersSolution();
+        System.out.println(solution.snakesAndLadders(board));
+    }
+}
+
+class SnakesAndLaddersSolution {
+    public int snakesAndLadders(int[][] board) {
+        int n = board.length;
+        int[] min_rolls = new int[n * n + 1];
+        Arrays.fill(min_rolls, -1);
+        Queue<Integer> q = new LinkedList<>();
+        min_rolls[1] = 0;
+        q.offer(1);
+
+        while (!q.isEmpty()) {
+            int x = q.poll();
+            for (int i = 1; i <= 6 && x + i <= n * n; i++) {
+                int t = x + i;
+                int row = (t - 1) / n;
+                int col = (t - 1) % n;
+                int v = board[n - 1 - row][(row % 2 == 1) ? (n - 1 - col) : col];
+                int y = (v > 0 ? v : t);
+                if (y == n * n) return min_rolls[x] + 1;
+                if (min_rolls[y] == -1) {
+                    min_rolls[y] = min_rolls[x] + 1;
+                    q.offer(y);
+                }
+            }
+        }
+        return -1;
+    }
+}
